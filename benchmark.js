9c9998eabb5ff4647d21eb7bae7e22ba5714abf8
isaacs <i@izs.me>
2015-04-29 11:25:21 -0400

    Use standard for styling and linting

--- benchmark.js
+++ benchmark.js
@@ -1,5 +1,5 @@
 var m = require('./minimatch.js')
-var pattern = "**/*.js"
+var pattern = '**/*.js'
 var expand = require('brace-expansion')
 var files = expand('x/y/z/{1..1000}.js')
 var start = process.hrtime()
@@ -8,8 +8,8 @@ for (var i = 0; i < 1000; i++) {
   for (var f = 0; f < files.length; f++) {
     var res = m(pattern, files[f])
   }
-  if (!(i%10)) process.stdout.write('.')
+  if (!(i % 10)) process.stdout.write('.')
 }
 console.log('done')
 var dur = process.hrtime(start)
-console.log('%s ms', dur[0]*1e3 + dur[1]/1e6)
+console.log('%s ms', dur[0] * 1e3 + dur[1] / 1e6)
--- test/extglob-ending-with-state-char.js
+++ test/extglob-ending-with-state-char.js
@@ -1,7 +1,7 @@
 var test = require('tap').test
 var minimatch = require('../')
 
-test('extglob ending with statechar', function(t) {
+test('extglob ending with statechar', function (t) {
   t.notOk(minimatch('ax', 'a?(b*)'))
   t.ok(minimatch('ax', '?(a*|b)'))
   t.end()
